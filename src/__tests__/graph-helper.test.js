f470ce7d6cc8511bda2e3c125e618d5d384bf1fb
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-05-13 01:23:42 +0200

    Add tests

---
+++ src/__tests__/graph-helper.test.js
@@ -0,0 +1,28 @@
+import 'babel-polyfill'
+import { convertFilesToTree } from '../graph-helper'
+
+test('construct basic tree', async () => {
+  expect(await convertFilesToTree([
+    'setup.py',
+    'project/main.py',
+    'project/utils.py',
+    'project/core/handler.py'
+  ])).toStrictEqual({
+    nodes: [
+      { id: '/setup.py', label: 'setup.py' },
+      { id: '/project', label: 'project' },
+      { id: '/project/main.py', label: 'main.py' },
+      { id: '/project/utils.py', label: 'utils.py' },
+      { id: '/project/core', label: 'core' },
+      { id: '/project/core/handler.py', label: 'handler.py' }
+    ],
+    edges: [
+      { id: undefined, source: '/', target: '/setup.py' },
+      { id: undefined, source: '/', target: '/project' },
+      { id: undefined, source: '/project', target: '/project/main.py' },
+      { id: undefined, source: '/project', target: '/project/utils.py' },
+      { id: undefined, source: '/project', target: '/project/core' },
+      { id: undefined, source: '/project/core', target: '/project/core/handler.py' }
+    ]
+  })
+})
