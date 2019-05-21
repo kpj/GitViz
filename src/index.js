4e786482b3a3379ab9b999bbc98fdeb6060eb0d4
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-05-21 22:01:52 +0200

    Add initial vue.js implementation

--- src/index.js
+++ src/index.js
@@ -1,84 +1,77 @@
-import 'babel-polyfill' // ReferenceError: regeneratorRuntime is not defined
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-import { parseGitRepository } from './git-handler'
-import { convertFilesToTree, Graph } from './graph-helper'
+import { createVisualization } from './visualization'
 
-function timer (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+Vue.use(Vuex)
 
-function formatCommit (commit) {
-  console.log(commit)
-  let date = new Date(commit.committer.timestamp * 1000)
+const store = new Vuex.Store({
+  state: {
+    repoUrl: 'https://github.com/kpj/GitViz',
+    inSetupState: true
+  },
+  mutations: {
+    repoUrl: (state, value) => {
+      state.repoUrl = value
+    },
+    finishSetup: state => {
+      state.inSetupState = false
+    }
+  }
+})
 
-  var year = date.getFullYear()
-  var month = (date.getMonth() + 1).toString().padStart(2, '0') // lol@+1
-  var day = date.getDate().toString().padStart(2, '0')
-  var hours = date.getHours().toString().padStart(2, '0')
-  var minutes = date.getMinutes().toString().padStart(2, '0')
-  var seconds = date.getSeconds().toString().padStart(2, '0')
-
-  let dateStr = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-
-  return `${dateStr} -- ${commit.message}`
-}
-
-// parsing
-document.getElementById('submitButton').addEventListener('click', () => {
-  const repoUrl = document.getElementById('repoUrlInputField').value
-  console.log('Parsing', repoUrl)
-
-  // rearrange view
-  document.getElementById('input').style.display = 'none'
-  document.getElementById('content').style.display = 'block'
-
-  // start procedure
-  parseGitRepository(
-    repoUrl
-  ).then(data => {
-    return Promise.all(data.map(async entry => {
-      return {
-        commit: entry['commit'],
-        tree: await convertFilesToTree(
-          entry['files'].filter(e => e.type !== 'remove')),
-        changedFiles: entry['files'].filter(e => e.type === 'modify')
-      }
-    }))
-  }).then(data => {
-    console.log(data)
-    console.log('Creating graph')
-
-    // setup network
-    let g = new Graph()
-    console.log(g)
-
-    // construct graph for first commit
-    let cur = data.shift()
-    g.addNodes(cur.tree.nodes)
-    g.addEdges(cur.tree.edges)
-
-    g.render()
-    document.getElementById('header').innerHTML = formatCommit(cur.commit)
-
-    // iterate over following commits
-    let delay = 2000
-    const iterate = async () => {
-      await timer(delay)
-      for (let cur of data) {
-        console.log(cur.commit.oid)
-        document.getElementById('header').innerHTML = formatCommit(cur.commit)
-
-        if (g.changeState(cur.tree.nodes, cur.tree.edges)) {
-          g.render()
-        }
-
-        for (let file of cur.changedFiles) {
-          g.blinkColor(file.path, 'red')
-        }
-
-        await timer(delay)
+const SetupDialog = {
+  template: `
+    <div id="setup">
+      <input v-model="repoUrl" size=100></input>
+      <button v-on:click="parseRepo">Parse repository</button>
+    </div>
+  `,
+  computed: {
+    repoUrl: {
+      get () {
+        return this.$store.state.repoUrl
+      },
+      set (value) {
+        this.$store.commit('repoUrl', value)
       }
     }
-    iterate()
-  })
+  },
+  methods: {
+    parseRepo: function () {
+      this.$store.commit('finishSetup')
+      createVisualization(store.state)
+    }
+  }
+}
+
+const NetworkView = {
+  template: `
+    <div>
+      <div id="header"></div>
+      <div id="container"></div>
+    </div>
+  `
+}
+
+const app = new Vue({
+  el: '#app',
+  store,
+  components: { SetupDialog, NetworkView },
+  template: `
+    <div>
+      <SetupDialog v-if="inSetupState"></SetupDialog>
+      <NetworkView v-if="!inSetupState"></NetworkView>
+    </div>
+  `,
+  computed: {
+    inSetupState: {
+      get () {
+        return this.$store.state.inSetupState
+      },
+      set (value) {
+        this.$store.commit('finishSetup')
+      }
+    }
+  }
 })
---
+++ src/visualization.js
@@ -0,0 +1,79 @@
+import 'babel-polyfill' // ReferenceError: regeneratorRuntime is not defined
+
+import { parseGitRepository } from './git-handler'
+import { convertFilesToTree, Graph } from './graph-helper'
+
+function timer (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function formatCommit (commit) {
+  console.log(commit)
+  let date = new Date(commit.committer.timestamp * 1000)
+
+  var year = date.getFullYear()
+  var month = (date.getMonth() + 1).toString().padStart(2, '0') // lol@+1
+  var day = date.getDate().toString().padStart(2, '0')
+  var hours = date.getHours().toString().padStart(2, '0')
+  var minutes = date.getMinutes().toString().padStart(2, '0')
+  var seconds = date.getSeconds().toString().padStart(2, '0')
+
+  let dateStr = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+
+  return `${dateStr} -- ${commit.message}`
+}
+
+// parsing
+export function createVisualization (state) {
+  console.log('State:', state)
+
+  // start procedure
+  parseGitRepository(
+    state.repoUrl
+  ).then(data => {
+    return Promise.all(data.map(async entry => {
+      return {
+        commit: entry['commit'],
+        tree: await convertFilesToTree(
+          entry['files'].filter(e => e.type !== 'remove')),
+        changedFiles: entry['files'].filter(e => e.type === 'modify')
+      }
+    }))
+  }).then(data => {
+    console.log(data)
+    console.log('Creating graph')
+
+    // setup network
+    let g = new Graph()
+    console.log(g)
+
+    // construct graph for first commit
+    let cur = data.shift()
+    g.addNodes(cur.tree.nodes)
+    g.addEdges(cur.tree.edges)
+
+    g.render()
+    document.getElementById('header').innerHTML = formatCommit(cur.commit)
+
+    // iterate over following commits
+    let delay = 2000
+    const iterate = async () => {
+      await timer(delay)
+      for (let cur of data) {
+        console.log(cur.commit.oid)
+        document.getElementById('header').innerHTML = formatCommit(cur.commit)
+
+        if (g.changeState(cur.tree.nodes, cur.tree.edges)) {
+          g.render()
+        }
+
+        for (let file of cur.changedFiles) {
+          g.blinkColor(file.path, 'red')
+        }
+
+        await timer(delay)
+      }
+    }
+    iterate()
+  })
+}
